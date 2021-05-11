522dba03a6abb752dc1f854f325f187ed7f1848f
Karan Purohit <76615863+purohit447@users.noreply.github.com>
2021-05-11 20:36:03 +0530

    Update 1.1 Important Singly Concepts.cpp

--- 1.1 Important Singly Concepts.cpp
+++ 1.1 Important Singly Concepts.cpp
@@ -10,7 +10,9 @@
 // Reversing K. Nodes;
 // Cycle in linked list (creating, detection and removal);
 // Hare and Tortoise algorithm  (Floyd's algorithm);
-
+// Calculating length of liked list;
+// Making two list intersect at node K;
+// Checking intersection in linked lists;
 
 
 //Source Code 
@@ -110,6 +112,89 @@ void remove_cycle(node* &head) // to remove cycle form linked list;
 	tortoise->next=NULL;
 }
 
+int length(node* head) // To calculate the length of a linked list;
+{
+    int l=0;
+    node* temp=head;
+    while(temp!=NULL)
+    {
+        l++;
+        temp=temp->next;
+    } return l;
+}
+
+void make_intersect(node* head_1, node* head_2,int pos)  // Make two linked list intersect;
+{
+    node* temp1=head_1;
+    pos--;
+    while(pos--)
+    {
+        temp1=temp1->next;
+    }
+    node* temp2=head_2;
+    while(temp2->next!=NULL)
+    {
+        temp2=temp2->next;
+    }
+    temp2->next=temp1;
+}
+
+int intersect(node* &head_1,node* &head_2)  // Checking for intersection in linked lists; 
+{
+    int l1=length(head_1);
+    int l2=length(head_2);
+    int d=0,count;
+    node* ptr1; node* ptr2;
+
+    if(l1>l2) // because both list end with same node results intersection point must present between first to last node of shoter list;
+    {
+        d=l1-l2;
+        ptr1=head_1;
+        ptr2=head_2;
+    }
+    else 
+    {
+        d=l2-l1;
+        ptr1=head_2;
+        ptr2=head_1;
+    }
+    count=d;
+    while(d)
+    {
+        ptr1=ptr1->next;
+        d--;
+    }
+
+    while(ptr1!=NULL && ptr2!=NULL)
+    {
+        if(ptr1==ptr2)
+        {
+            return count;
+        }
+        ptr1=ptr1->next;
+        ptr2=ptr2->next;
+        count++;
+    } 
+
+    return 0;
+}
+
+void odd_then_even(node* &head)
+{
+	node* odd=head;
+	node* even=head->next;
+	node* even_start=even;
+	while(odd->next!=NULL && even->next!=NULL)
+	{
+		odd->next=even->next;
+		odd=odd->next;
+		even->next=odd->next;
+		even=even->next;
+	}
+	if(odd->next!=NULL) even->next=NULL;
+	odd->next=even_start;
+}
+
 
 //-------------------------------------- MAIN FUNCTION LIES HERE -----------------------------------------//
 
@@ -144,7 +229,27 @@ int main()
 
 	display(new_head); // Display list   output:  1->2->3->4->5->6->7->NULL;
 
-	return 0;
+	node* head_2=NULL;  // intitalizing 2nd list;
+
+	tail(head_2,8);   // insert at tail    results(8->null);    results(8->NULL);
+	tail(head_2,4);	// insert at tail    results(8->4->null);   results(8->4->NULL);
+	tail(head_2,3);	// insert at tail    results(8->4->3->null);  results(8->4->3->NULL);
+
+	display(head_2); // display list before intersection   output : 8->4->3->NULL;
+
+	make_intersect(new_head,head_2,3);  // intersecting 2nd list with 1st at 3rd node; 
+    display(head_2); // display 2nd list after intersection    output : 8->4->3->3->4->5->6->7->NULL;
+
+    if(intersect(head_2,new_head)!=0) cout<<"Inserting at node "<<intersect(head_2,new_head)<<endl; // checking for intersection;  
+    else cout<<"not intersecting\n";
+
+    // output : Inserting at node 3;
+
+    display(new_head); // dispay list before changing nodes;   output :  1->2->3->4->5->6->7->NULL;
+    odd_then_even(new_head); // arranging even nodes after odd nodes;  
+    display(new_head); // display arranged list    output : 1->3->5->7->2->4->6->NULL;
+
+    return 0;
 }
 
 
